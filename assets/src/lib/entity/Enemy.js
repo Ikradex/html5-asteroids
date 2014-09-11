b3a4a14e72c2589a5e816382ccc7d82447123ac4
Ikradex <ikradex@gmail.com>
2014-09-11 23:18:13 +0100

    Improved aiming functions in Enemy class

--- assets/src/lib/entity/Enemy.js
+++ assets/src/lib/entity/Enemy.js
@@ -1,8 +1,10 @@
 /*global define, canvas, ctx*/
 
 define("Enemy", [
-    "CollidableEntity"
-], function (CollidableEntity) {
+    "CollidableEntity",
+    "Vector2D",
+    "Library"
+], function (CollidableEntity, Vector2D, Library) {
     "use strict";
 
     Enemy.inherits([CollidableEntity]);
@@ -13,6 +15,8 @@ define("Enemy", [
         if (!(this instanceof Enemy)) {
             throw new TypeError("Enemy constructor cannot be called as a function.");
         }
+
+        this._accuracy = 0.1;
     }
 
     Enemy.prototype.update = function (dt) {
@@ -20,6 +24,50 @@ define("Enemy", [
             this._updatePosition(dt);
             this._weapon.update(dt);
         }
+    };
+
+    Enemy.prototype.getNearestEntity = function (entities) {
+        var index = -1,
+            max = 0;
+
+        // select closest player
+        for (var i = 0; i < entities.length; i++) {
+            var entity = entities[i];
+
+            if (entity.getDestroyed()) {
+                continue;
+            }
+
+            var distance = this.getPos().distanceTo(entity.getPos());
+
+            if (distance > max) {
+                index = i;
+                max = distance;
+            }
+        }
+
+        return (index >= 0) ? entities[index] : null;
+    };
+
+    Enemy.prototype.aim = function (entity) {
+        var predictedPos, predictedAngle;
+
+        if (entity !== null) {
+            var relativeVelocity = entity.getVelocity().add(this.getVelocity().scale(0.5));
+
+            predictedPos = entity.getPos().add(relativeVelocity);
+            predictedAngle = this.getPos().angleTo(predictedPos);
+
+            // add inaccuracies
+            var inaccuracy = predictedAngle - (predictedAngle * this.getAccuracy());
+            predictedAngle += Library.toRadians(Library.randomInteger(-inaccuracy, inaccuracy));
+        } else {
+            // default to random aiming if no entity to aim at
+            predictedAngle = Library.randomInteger(-180, 180);
+        }
+
+        // return the direction the Saucer will need to face
+        return new Vector2D(Math.cos(predictedAngle), Math.sin(predictedAngle));
     };
 
     Enemy.prototype.shoot = function (dt) {
@@ -30,8 +78,16 @@ define("Enemy", [
         return this._weapon;
     };
 
+    Enemy.prototype.getAccuracy = function () {
+        return this._accuracy;
+    };
+
     Enemy.prototype.setWeapon = function (weapon) {
         this._weapon = weapon;
+    };
+
+    Enemy.prototype.setAccuracy = function (accuracy) {
+        this._accuracy = accuracy;
     };
 
     return Enemy;
--- assets/src/lib/entity/Saucer.js
+++ assets/src/lib/entity/Saucer.js
@@ -1,78 +1,77 @@
 /*global define, canvas, ctx*/
 
 define("Saucer", [
-        "Library",
-        "Vector2D",
-        "EventTimer",
-        "Enemy",
-        "Cannon"
-    ],
-    function (Library, Vector2D, EventTimer, Enemy, Cannon) {
-        "use strict";
+    "Library",
+    "Vector2D",
+    "EventTimer",
+    "Enemy",
+    "Cannon"
+], function (Library, Vector2D, EventTimer, Enemy, Cannon) {
+    "use strict";
 
-        Saucer.inherits([Enemy]);
+    Saucer.inherits([Enemy]);
 
-        function Saucer(x, y, width, height, scoreValue) {
-            Enemy.apply(this, [x, y, width, height, Saucer.INIT_MASS, scoreValue]);
+    function Saucer(x, y, width, height, scoreValue) {
+        Enemy.apply(this, [x, y, width, height, Saucer.INIT_MASS, scoreValue]);
 
-            if (!(this instanceof Saucer)) {
-                throw new TypeError("Saucer constructor cannot be called as a function.");
-            }
-
-            this._dt = 0.016; // store the value of dt for automated shooting
-
-            var cannon = new Cannon();
-            this.setWeapon(cannon);
-
-            this._fireTimer = new EventTimer(this._getFireInterval(), function () {
-                this.shoot(this._dt);
-                this._fireTimer.setWaitTime(this._getFireInterval());
-            }.bind(this));
+        if (!(this instanceof Saucer)) {
+            throw new TypeError("Saucer constructor cannot be called as a function.");
         }
 
-        Saucer.X_VELOCITY = 75;
-        Saucer.MIN_FIRE_INTERVAL = 500;
-        Saucer.MAX_FIRE_INTERVAL = 2000;
-        Saucer.INIT_MASS = 400;
+        this._dt = 0.016; // store the value of dt for automated shooting
 
-        Saucer.prototype.update = function (dt) {
-            this._fireTimer.wait(dt);
-            this._updatePosition(dt);
-            this._weapon.update(dt);
+        var cannon = new Cannon();
+        this.setWeapon(cannon);
 
-            this._dt = dt;
-        };
+        this._fireTimer = new EventTimer(this._getFireInterval(), function () {
+            this.shoot(this._dt);
+            this._fireTimer.setWaitTime(this._getFireInterval());
+        }.bind(this));
+    }
 
-        Saucer.prototype.render = function () {
-            ctx.save();
-            ctx.fillStyle = "#F00";
-            ctx.beginPath();
-            ctx.arc(this.getPos().x, this.getPos().y, this.getDimensions().width / 2, 0, 2 * Math.PI, false);
-            ctx.stroke();
-            ctx.fill();
-            ctx.restore();
-        };
+    Saucer.X_VELOCITY = 75;
+    Saucer.MIN_FIRE_INTERVAL = 500;
+    Saucer.MAX_FIRE_INTERVAL = 2000;
+    Saucer.INIT_MASS = 400;
 
-        // Override Enemy.shoot
-        Saucer.prototype.shoot = function (dt) {
-            this._dir = this._randomDir();
-            this._weapon.fire(this, this.getPos().x, this.getPos().y, this.getVelocity(), this._dir, dt);
-        };
+    Saucer.prototype.update = function (dt) {
+        this._fireTimer.wait(dt);
+        this._updatePosition(dt);
+        this._weapon.update(dt);
 
-        Saucer.prototype._getFireInterval = function () {
-            return Library.randomInteger(Saucer.MIN_FIRE_INTERVAL, Saucer.MAX_FIRE_INTERVAL);
-        };
+        this._dt = dt;
+    };
 
-        Saucer.prototype._randomDir = function () {
-            var randAngle = Library.randomInteger(-180, 180);
+    Saucer.prototype.render = function () {
+        ctx.save();
+        ctx.fillStyle = "#F00";
+        ctx.beginPath();
+        ctx.arc(this.getPos().x, this.getPos().y, this.getDimensions().width / 2, 0, 2 * Math.PI, false);
+        ctx.stroke();
+        ctx.fill();
+        ctx.restore();
+    };
 
-            return new Vector2D(-Math.cos(randAngle), -Math.sin(randAngle));
-        };
+    // Override Enemy.shoot
+    Saucer.prototype.shoot = function (dt) {
+        this._dir = this._aim();
+        this._weapon.fire(this, this.getPos().x, this.getPos().y, this.getVelocity(), this._dir, dt);
+    };
 
-        // Override Entity._handleOutOfBounds
-        Saucer.prototype._handleOutOfBounds = function () {
-            this.destroy();
-        };
+    Saucer.prototype._getFireInterval = function () {
+        return Library.randomInteger(Saucer.MIN_FIRE_INTERVAL, Saucer.MAX_FIRE_INTERVAL);
+    };
 
-        return Saucer;
-    });
+    Saucer.prototype._randomDir = function () {
+        var randAngle = Library.randomInteger(-180, 180);
+
+        return new Vector2D(-Math.cos(randAngle), -Math.sin(randAngle));
+    };
+
+    // Override Entity._handleOutOfBounds
+    Saucer.prototype._handleOutOfBounds = function () {
+        this.destroy();
+    };
+
+    return Saucer;
+});
