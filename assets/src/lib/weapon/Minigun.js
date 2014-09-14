a15511d7a72356be15f54e4bf75eef143e458fa3
Ikradex <ikradex@gmail.com>
2014-09-14 19:07:47 +0100

    Updated Minigun to new Weapon API

--- assets/src/lib/weapon/Minigun.js
+++ assets/src/lib/weapon/Minigun.js
@@ -5,8 +5,8 @@ define("Minigun", ["Weapon", "Bullet"], function (Weapon, Bullet) {
 
     Minigun.inherits([Weapon]);
 
-    function Minigun() {
-        Weapon.apply(this, arguments);
+    function Minigun(x, y) {
+        Weapon.apply(this, [x, y]);
 
         if (!(this instanceof Minigun)) {
             throw new TypeError("Minigun constructor cannot be called as a function.");
@@ -25,9 +25,11 @@ define("Minigun", ["Weapon", "Bullet"], function (Weapon, Bullet) {
         }
     };
 
-    Minigun.prototype.fire = function (shooter, x, y, velocity, dir, dt) {
-        var bullet = new Bullet(shooter, x, y, velocity),
-            force = this._propelProjectile(bullet, dir, dt);
+    Minigun.prototype.fire = function (shooter, velocity, dir, dt) {
+        var bullet = new Bullet(shooter, this.getPos().x, this.getPos().y, velocity);
+        bullet.setMaxTravelDistance(this.getProjectileMaxTravelDistance());
+
+        var force = this._propelProjectile(bullet, dir, dt);
 
         return force;
     };
