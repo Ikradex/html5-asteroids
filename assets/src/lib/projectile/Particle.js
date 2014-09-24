b55cc3956df3778dd21af5bddcb0cb815c2a9424
Ikradex <ikradex@gmail.com>
2014-09-24 10:01:46 +0100

    Particle now inherits CollidableEntity

--- assets/src/lib/projectile/Particle.js
+++ assets/src/lib/projectile/Particle.js
@@ -1,28 +1,26 @@
 /*global define, canvas, ctx*/
 
 define("Particle", [
-    "Entity",
+    "CollidableEntity",
     "Vector2D"
-], function (Entity, Vector2D) {
+], function (CollidableEntity, Vector2D) {
     "use strict";
 
-    Particle.inherits([Entity]);
+    Particle.inherits([CollidableEntity]);
 
     function Particle(x, y, wh, mass) {
-        Entity.apply(this, [x, y, wh, wh, mass, 0]);
+        CollidableEntity.apply(this, [x, y, wh, wh, mass, 0]);
 
         if (!(this instanceof Particle)) {
             throw new TypeError("Particle constructor cannot be called as a function.");
         }
-
-        this._destroyed = false;
 
         this._travelDistance = new Vector2D(0, 0);
 
         var corner = new Vector2D(0, 0),
             center = new Vector2D(canvas.width / 2, canvas.height / 2);
 
-        this._maxTravelDistance = center.distanceTo(corner) * 1;
+        this._maxTravelDistance = center.distanceTo(corner);
     }
 
     Particle.prototype.update = function (dt) {
@@ -30,7 +28,7 @@ define("Particle", [
             this._updatePosition(dt);
             this._travelDistance = this._travelDistance.add(this.getVelocity().scale(dt));
         } else {
-            this._destroyed = true;
+            this.setDestroyed(true);
         }
     };
 
@@ -39,6 +37,28 @@ define("Particle", [
         ctx.fillStyle = "#FFF";
         ctx.fillRect(this.getPos().x, this.getPos().y, this.getDimensions().width, this.getDimensions().height);
         ctx.restore();
+    };
+
+    // Override CollidableEntity.attracts
+    Particle.prototype.attracts = function (entity) {
+        var valid = true;
+
+        valid = (entity != this && this.distanceTo(entity) > 1);
+
+        if (valid) {
+            var force = this._getGravityForce(entity);
+            this.applyForce(force.scale(Game.PHYSICS_LEVEL));
+        }
+    };
+
+    Particle.prototype.intersects = function (entity) {
+        var valid = true;
+
+        valid = (entity != this);
+
+        if (valid) {
+            this.setDestroyed(true);
+        }
     };
 
     return Particle;
